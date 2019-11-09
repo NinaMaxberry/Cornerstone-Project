0930417b35c207b9946ef293ca8ec7940a5e701a
Nina Maxberry <nmax@DESKTOP-R9KQV1C.nmaxberry@yahoo.com>
2019-11-09 17:10:13 -0500

    create class/pagee

--- src/zip/views.py
+++ src/zip/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse # must add to integrate django
 from django.shortcuts import render
 
 # Create your views here.
+def homepage_view(*args, **kwargs):
+    return HttpResponse("<h1>What voting district do you live in?</h1>") #Can use str HTML with django function
